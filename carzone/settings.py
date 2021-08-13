8c9fdf2ada24a9574a8daf1adc81a089f9f3e7cd
niloynjr <86718842+niloynjr@users.noreply.github.com>
2021-08-13 22:00:51 +0600

    update settings.py

--- carzone/settings.py
+++ carzone/settings.py
@@ -156,9 +156,8 @@ SITE_ID = 1
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'niloy.njr@gmail.com'
-EMAIL_HOST_PASSWORD = 'nibedita143@#$'
+EMAIL_HOST_PASSWORD = 'niloyjyoti170642'
 EMAIL_USE_TLS = True
-
 
 
 
